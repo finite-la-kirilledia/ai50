3d5adcde78389491f6f8c5ac89722e2c7cb3a7ed
Kirill Minko <kirillmi@playtika.com>
2021-10-24 18:31:21 +0300

    crossword: solution

--- optimization/crossword/generate.py
+++ optimization/crossword/generate.py
@@ -141,56 +141,11 @@ class CrosswordCreator():
             if self.revise(x, y):
                 if len(self.domains[x]) == 0:
                     return False
-                for neighbor in self.crossword.neighbors(x):
-                    if neighbor not in self.domains[y]:
-                        arc = (x, neighbor)
-                        arcs.append(arc)
+                for neighbor in self.crossword.neighbors(x) - self.domains[y]:
+                    arc = (x, neighbor)
+                    arcs.append(arc)
 
         return True
-
-    def assignment_complete(self, assignment):
-        """
-        Return True if `assignment` is complete (i.e., assigns a value to each
-        crossword variable); return False otherwise.
-        """
-        raise NotImplementedError
-
-    def consistent(self, assignment):
-        """
-        Return True if `assignment` is consistent (i.e., words fit in crossword
-        puzzle without conflicting characters); return False otherwise.
-        """
-        raise NotImplementedError
-
-    def order_domain_values(self, var, assignment):
-        """
-        Return a list of values in the domain of `var`, in order by
-        the number of values they rule out for neighboring variables.
-        The first value in the list, for example, should be the one
-        that rules out the fewest values among the neighbors of `var`.
-        """
-        raise NotImplementedError
-
-    def select_unassigned_variable(self, assignment):
-        """
-        Return an unassigned variable not already part of `assignment`.
-        Choose the variable with the minimum number of remaining values
-        in its domain. If there is a tie, choose the variable with the highest
-        degree. If there is a tie, any of the tied variables are acceptable
-        return values.
-        """
-        raise NotImplementedError
-
-    def backtrack(self, assignment):
-        """
-        Using Backtracking Search, take as input a partial assignment for the
-        crossword and return a complete assignment if possible to do so.
-
-        `assignment` is a mapping from variables (keys) to words (values).
-
-        If no assignment is possible, return None.
-        """
-        raise NotImplementedError
 
     def init_arcs(self):
         arcs = []
@@ -202,6 +157,87 @@ class CrosswordCreator():
                     arcs.append(arc)
 
         return arcs
+
+    def assignment_complete(self, assignment):
+        """
+        Return True if `assignment` is complete (i.e., assigns a value to each
+        crossword variable); return False otherwise.
+        """
+        for var in self.crossword.variables:
+            if var not in assignment:
+                return False
+
+        return True
+
+    def consistent(self, assignment):
+        """
+        Return True if `assignment` is consistent (i.e., words fit in crossword
+        puzzle without conflicting characters); return False otherwise.
+        """
+        domain_values = assignment.values()
+        if len(domain_values) != len(set(domain_values)):
+            return False
+
+        for var in assignment:
+            for neighbor in self.crossword.neighbors(var):
+                if neighbor in assignment:
+                    i, j = self.crossword.overlaps[var, neighbor]
+                    if assignment[var][i] != assignment[neighbor][j]:
+                        return False
+
+        return True
+
+    def order_domain_values(self, var, assignment):
+        """
+        Return a list of values in the domain of `var`, in order by
+        the number of values they rule out for neighboring variables.
+        The first value in the list, for example, should be the one
+        that rules out the fewest values among the neighbors of `var`.
+        """
+        return self.domains[var]
+
+    def select_unassigned_variable(self, assignment):
+        """
+        Return an unassigned variable not already part of `assignment`.
+        Choose the variable with the minimum number of remaining values
+        in its domain. If there is a tie, choose the variable with the highest
+        degree. If there is a tie, any of the tied variables are acceptable
+        return values.
+        """
+        best_var = None
+        for var in self.crossword.variables:
+            if var not in assignment:
+                if (
+                        best_var is None or
+                        len(self.domains[var]) < len(self.domains[best_var]) or
+                        len(self.crossword.neighbors(var)) > len(self.crossword.neighbors(best_var))
+                ):
+                    best_var = var
+
+        return best_var
+
+    def backtrack(self, assignment):
+        """
+        Using Backtracking Search, take as input a partial assignment for the
+        crossword and return a complete assignment if possible to do so.
+
+        `assignment` is a mapping from variables (keys) to words (values).
+
+        If no assignment is possible, return None.
+        """
+        if self.assignment_complete(assignment):
+            return assignment
+
+        var = self.select_unassigned_variable(assignment)
+        for domain_value in self.order_domain_values(var, assignment):
+            assignment[var] = domain_value
+            if self.consistent(assignment):
+                result = self.backtrack(assignment)
+                if result is not None:
+                    return result
+            del assignment[var]
+
+        return None
 
 
 def main():
